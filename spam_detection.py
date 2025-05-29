ae8a8287ccd8443c06c07387fa2a81852bc1fda6
Sunyasloka <sunyaslokapriyadarshi@gmail.com>
2025-05-30 01:37:23 +0530

    Add files via upload

---
+++ spam_detection.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+# Load model and vectorizer
+model = pickle.load(open('spam_model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+st.title("Spam Detection App")
+input_text = st.text_area("Enter a message:")
+
+if st.button("Predict"):
+    transformed_text = vectorizer.transform([input_text]).toarry()
+    result = model.predict(transformed_text)
+    st.write("Prediction:", "Spam" if result[0] == 1 else "Ham")
